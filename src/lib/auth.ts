80fe62aa51ac72f88bf0e5b323eef10e7b5ab442
Jed Patterson <jedpatterson123@gmail.com>
2025-03-05 10:13:36 +0000

    init auth & db config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,13 @@
 import { betterAuth } from "better-auth";
+import { twoFactor, admin } from "better-auth/plugins";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "./db";
 
 export const auth = betterAuth({
+  plugins: [twoFactor(), admin()],
+  database: drizzleAdapter(db, {
+    provider: "mysql",
+  }),
   emailAndPassword: {
     enabled: true,
   },
