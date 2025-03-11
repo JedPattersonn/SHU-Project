3d0bc45b28511729d6ce8342a2493b69000adcda
Jed Patterson <jedpatterson123@gmail.com>
2025-03-11 16:50:43 +0000

    anomaly card

--- web/src/app/(authenticated)/page.tsx
+++ web/src/app/(authenticated)/page.tsx
@@ -1,7 +1,13 @@
+import { AnomalyAlertCard } from "@/components/dashboard/anomaly-alert-card";
 import { StatCard } from "@/components/dashboard/stat-card";
 import { auth } from "@/lib/auth";
+import { Metadata } from "next";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "Dashboard"
+}
 
 export default async function Home() {
   const session = await auth.api.getSession({
@@ -13,11 +19,14 @@ export default async function Home() {
   }
 
   return (
+    <div className="flex flex-col gap-4">
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
       <StatCard title="Total Active Connections" value={100} />
       <StatCard title="Total Active Connections" value={100} />
       <StatCard title="Total Active Connections" value={100} />
       <StatCard title="Total Active Connections" value={100} />
+    </div>
+    <AnomalyAlertCard />
     </div>
   );
 }
