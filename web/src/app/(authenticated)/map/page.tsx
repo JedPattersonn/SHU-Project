960f7f8671fc9a3a0dc2e5503fc5fcc35e5a76d6
Jed Patterson <jedpatterson123@gmail.com>
2025-03-11 14:27:38 +0000

    move to web

---
+++ web/src/app/(authenticated)/map/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import InteractiveHeatMap from '@/components/map/interactive-map';
+
+function App() {
+  return (
+    <div>
+      <InteractiveHeatMap />
+    </div>
+  );
+}
+
+export default App;
