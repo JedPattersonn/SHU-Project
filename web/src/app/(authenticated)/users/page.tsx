7a1e432608a0de974b1e5af16709260fff5495b1
mocanul <bdinulescu85@gmail.com>
2025-03-18 14:54:07 +0000

    add user query

--- web/src/app/(authenticated)/users/page.tsx
+++ web/src/app/(authenticated)/users/page.tsx
@@ -1,7 +1,15 @@
-
-
+import { user as UserSchema } from "@/lib/db/schema"
+import { db } from "@/lib/db"
 export default async function UsersPage() {
+
+const users = await db.select().from(UserSchema)
+
+console.log(users)
+
+
     return (
         <p>Users</p>
     )
 }
+
+
