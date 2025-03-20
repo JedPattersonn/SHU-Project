a87342947f64199c0f7683c557804c6c94aa431e
Jed Patterson <jedpatterson123@gmail.com>
2025-03-20 15:44:19 +0000

    create user funtionality

---
+++ web/src/components/users/users-table.tsx
@@ -0,0 +1,41 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+interface TableUser {
+  id: string;
+  name: string;
+  email: string;
+  role: string | null;
+}
+
+export function UsersTable({ users }: { users: TableUser[] }) {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>ID</TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Email</TableHead>
+          <TableHead>Role</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {users.map((user) => (
+          <TableRow key={user.id}>
+            <TableCell>{user.id}</TableCell>
+            <TableCell>{user.name}</TableCell>
+            <TableCell>{user.email}</TableCell>
+            <TableCell>{user.role}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
